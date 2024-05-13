cccacfba2093f7f41a2721590a854ad8e7c546be
anke <245783065@qq.com>
2024-05-13 14:40:43 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('eslint-config-airbnb'),
+    require.resolve('eslint-config-airbnb/hooks'),
+    require.resolve('eslint-config-airbnb-typescript')
+  ],
+  rules: {
+    'react/function-component-definition': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/exhaustive-deps': 1,
+    'import/no-extraneous-dependencies': [
+      0,
+      {
+        devDependencies: true,
+        peerDependencies: true
+        // optionalDependencies: true,
+        // bundledDependencies: true
+      }
+    ]
+  },
+  parser: require.resolve('@typescript-eslint/parser'),
+  parserOptions: {
+    project: require.resolve('./tsconfig.json')
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+    // 'import/resolver': {
+    //   alias: {
+    //     map: [['@', './src']],
+    //     extensions: ['.ts', '.tsx']
+    //   }
+    // }
+  }
+};
