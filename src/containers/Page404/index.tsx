e260e80f4562dc805f4fb94db5e1f1e300b18564
anke <245783065@qq.com>
2024-05-21 16:39:28 +0800

    feat: 完成路由搭建，删除无用页面

---
+++ src/containers/Page404/index.tsx
@@ -0,0 +1,14 @@
+import { Button, Result } from 'antd';
+/**
+* 404页面
+*/
+const Page404 = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="您访问的页面不存在"
+    extra={<Button type="primary" href="/">返回首页</Button>}
+  />
+);
+
+export default Page404;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './utils/apollo';
-import App from './App';
 import './index.css';
+import { ROUTE_CONFIG } from './routes';
+import Page404 from './containers/Page404';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {ROUTE_CONFIG.map((item) => (
+          <Route
+            path={item.path}
+            key={item.key}
+            element={<item.element />}
+          />
+        ))}
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   </ApolloProvider>,
 );
