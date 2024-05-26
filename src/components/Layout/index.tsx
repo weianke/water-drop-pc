7b43d16d3d6b97d111a5af75bc9591a9873b8625
anke <245783065@qq.com>
2024-05-26 17:59:50 +0800

    feat: 完成layout 退出功能

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,8 @@
 import { useUserContext } from '@/hooks/userHooks';
 import { ROUTE_CONFIG } from '@/routes';
+import { AUTH_TOKEN } from '@/utils/constants';
 import { MenuDataItem, PageContainer, ProLayout } from '@ant-design/pro-components';
-import { Link, useOutlet } from 'react-router-dom';
+import { Link, useNavigate, useOutlet } from 'react-router-dom';
 
 const menuItemRender = (item: MenuDataItem, dom: React.ReactNode) => (
   <Link to={item.path || '/'}>{dom}</Link>
@@ -12,11 +13,19 @@ const menuItemRender = (item: MenuDataItem, dom: React.ReactNode) => (
 const Layout = () => {
   const outlet = useOutlet();
   const { store } = useUserContext();
+  const nav = useNavigate();
+
+  const logout = () => {
+    localStorage.setItem(AUTH_TOKEN, '');
+    sessionStorage.setItem(AUTH_TOKEN, '');
+    nav('/login');
+  };
+
   return (
     <ProLayout
       layout="mix"
       siderWidth={130}
-      avatarProps={{ src: 'global/logo.svg', title: store.tel, size: 'small' }}
+      avatarProps={{ src: 'global/logo.svg', title: store.tel, size: 'small', onClick: logout }}
       title={false}
       logo={
         <img
@@ -24,6 +33,7 @@ const Layout = () => {
           alt="logo"
         />
       }
+      onMenuHeaderClick={() => nav('/')}
       route={{
         path: '/',
         routes: ROUTE_CONFIG
